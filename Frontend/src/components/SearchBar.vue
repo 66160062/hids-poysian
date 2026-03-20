a95c4f2f3cf86d623e3d8067489a355f410bf581
Teepakorn Klinpanao <66160062@go.buu.ac.th>
2026-03-20 16:08:30 +0700

    fix(InspectionPage): add TypeScript support to script setup

--- Frontend/src/components/SearchBar.vue
+++ Frontend/src/components/SearchBar.vue
@@ -1,28 +1,13 @@
 <template>
   <div class="row items-center q-gutter-x-sm">
-    <q-input
-      outlined
-      dense
-      class="col"
-      bg-color="white"
-      :model-value="modelValue"
-      @update:model-value="$emit('update:modelValue', $event)"
-      placeholder="ค้นหาประเภทห้อง, ประเภทห้องย่อย"
-    >
+    <q-input outlined dense class="col" bg-color="white" :model-value="modelValue"
+      @update:model-value="$emit('update:modelValue', $event)" placeholder="ค้นหาประเภทห้อง, ประเภทห้องย่อย">
       <template v-slot:prepend>
         <q-icon name="search" color="grey-6" />
       </template>
     </q-input>
 
-    <q-btn
-      round
-      outline
-      color="grey-4"
-      text-color="grey-7"
-      icon="tune"
-      class="bg-white"
-      @click="$emit('filter')"
-    />
+    <q-btn round outline color="grey-4" text-color="grey-7" icon="tune" class="bg-white" @click="$emit('filter')" />
   </div>
 </template>
 
